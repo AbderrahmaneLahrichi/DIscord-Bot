09f35a759447f01acebe96938b2f7cf01034df3f
AbderrahmaneLahrichi <111699655+AbderrahmaneLahrichi@users.noreply.github.com>
2023-03-15 12:17:02 -0700

    initial commit

---
+++ DiscordBot.js
@@ -0,0 +1,47 @@
+const{ Client, GatewayIntentBits ,EmbedBuilder, PermisionsBitField, Permissions, ActivityType} = require("discord.js");
+const prefix = "!";
+const client = new Client({intents:[GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+const axios = require('axios')
+
+     
+client.on("ready", () =>{
+
+    client.user.setActivity(`playing with cats and dogs`, {type:"watching"}); 
+  
+
+})
+client.on("messageCreate", (message) =>{
+  if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+  
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+
+// This will generate the random dog image.
+  if (command === "dog") {
+    axios.get('https://api.thedogapi.com/v1/images/search')
+    .then(response =>{
+      message.channel.send(response.data[0].url)
+})
+    .catch(console.error)
+  }
+//This will generate the random cat image
+  if (command === "cat") {
+    axios.get('https://api.thecatapi.com/v1/images/search')
+    .then(response =>{
+      message.channel.send(response.data[0].url)
+})
+    .catch(console.error)
+  }
+ })
+
+
+
+
+
+client.login("MTAzNDYzMzYyNDcxNDIxNTQzNg.GK57_k.vDflp5OyzHD2lwSHfMbzvW3yXkW70xDQCX510k");
+
+
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# Discord Bot
+
+Here we have a discord bot that displays a randomized cat or do image depending on Whether you use the !dog or !cat  commands. We also consider if the user accidentally used a capital letter int their spelling. 
+
+
+
+
+## Demo Video
+![Discord_Bot_gif](https://user-images.githubusercontent.com/111699655/225404682-e83a4758-468b-4fa1-8689-72ec813400e0.gif)
+
+
+
+# Future game updates:
+In the future I am looking to make the discord bot more versatile and allow  for admins to  kick players, ban player, put players in time out, play mini games, and  give statistics of the server. I am looking to make this discord bot used worldwide. 
